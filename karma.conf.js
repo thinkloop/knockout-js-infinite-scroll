b34b6c19f02d714504f428d0163f3e5980a70ab4
Baz <bk@thinkloop.com>
2014-08-14 00:00:55 -0400

    Renamed infinitescroll.js to knockout-js-infinite-scroll.js and fixed travis/karma/coveralls tests and logging

--- karma.conf.js
+++ karma.conf.js
@@ -13,20 +13,19 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'bower_components/commonjs-require-definition/require.js',
-      'bower_components/knockout.js/knockout.js',
+      'bower_components/knockout/dist/knockout.js',
       'bower_components/mocha/mocha.js',
       'bower_components/chai/chai.js',
       'bower_components/sinon-chai/lib/sinon-chai.js',
       'bower_components/sinon/lib/sinon.js',
-      'infinitescroll.js',
+      'knockout-js-infinite-scroll.js',
       'tests.js'
     ],
 
     // list of files to exclude
     exclude: [
-      
-    ],
 
+    ],
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
@@ -35,7 +34,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'infinitescroll.js': 'coverage'
+      'knockout-js-infinite-scroll.js': 'coverage'
     },
 
     // test results reporter to use
@@ -48,10 +47,12 @@ module.exports = function(config) {
         ]
     },
 
+    coverallsReporter: {
+        repoToken: "BMoZLkpnTVMTl8BzixWMIdxYavV5xPl3G"
+    },
 
     // web server port
     port: 9876,
-
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
@@ -61,22 +62,18 @@ module.exports = function(config) {
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
 
-
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
 
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
